950ba5fe6e0c0263756255d2d571b7f41fc45dc6
artahian <artahian@gmail.com>
2025-05-25 19:33:08 -0700

    Store chats and messages

--- ai-chat/src/server/ai-chat/index.ts
+++ ai-chat/src/server/ai-chat/index.ts
@@ -1,12 +1,23 @@
-import { getConfig, Module } from 'modelence/server';
+import { getConfig, Module, ObjectId, UserInfo } from 'modelence/server';
 import { generateText } from 'ai';
 import { createOpenAI } from '@ai-sdk/openai';
+import { dbChats, dbMessages } from './db';
 import { z } from 'zod';
 
 export default new Module('aiChat', {
+  stores: [dbChats, dbMessages],
+  queries: {
+    async getChats(args, { user: _user }) {
+      const user = requireUser(_user);
+      return dbChats.fetch({ userId: new ObjectId(user.id) });
+    },
+  },
   mutations: {
-    async generateResponse(args) {
-      const { messages } = z.object({
+    async generateResponse(args, { user: _user }) {
+      const user = requireUser(_user);
+
+      const { chatId: existingChatId, messages } = z.object({
+        chatId: z.string().optional(),
         messages: z.array(z.object({
           role: z.enum(['user', 'assistant']),
           content: z.string(),
@@ -21,6 +32,22 @@ export default new Module('aiChat', {
         messages: contextMessages,
       });
 
+      const lastMessage = messages[messages.length - 1];
+      const chatId = existingChatId ?? await createNewChat(user, lastMessage.content);
+      await dbMessages.insertMany([{
+        userId: new ObjectId(user.id),
+        chatId: new ObjectId(chatId),
+        role: 'user',
+        content: lastMessage.content,
+        createdAt: new Date(Date.now() - 1),
+      }, {
+        userId: new ObjectId(user.id),
+        chatId: new ObjectId(chatId),
+        role: 'assistant',
+        content: response.text,
+        createdAt: new Date(),
+      }]);
+
       return {
         role: 'assistant',
         content: response.text,
@@ -28,3 +55,19 @@ export default new Module('aiChat', {
     },
   },
 });
+
+async function createNewChat(user: UserInfo, firstMessage: string) {
+  const { insertedId } = await dbChats.insertOne({
+    userId: new ObjectId(user?.id),
+    title: firstMessage.length > 20 ? firstMessage.slice(0, 20) + '...' : firstMessage,
+    createdAt: new Date(),
+  });
+  return insertedId.toString();
+}
+
+function requireUser(user: UserInfo | null): UserInfo {
+  if (!user) {
+    throw new Error('Unauthorized');
+  }
+  return user;
+}
